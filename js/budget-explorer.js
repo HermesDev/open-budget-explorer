318a5315317e58173609e86fab6b8023cac96322
FlorianGoussin <florian.goussin@gmail.com>
2015-07-24 08:01:57 -0600

    clean the budget-explorer js file

--- js/budget-explorer.js
+++ js/budget-explorer.js
@@ -9,31 +9,20 @@ var dataCountTop    = dc.dataCount("#data-count-top"),
     fundRowChart    = dc.rowChart("#chart-row-fund");
 
 // LOAD OUR DATA
-// use static or load via d3.csv("spendData.csv", function(error, spendData) {/* do stuff */});
-
 var myData = d3.csv('data/14budget.csv', function(budgetData){
-    //            var numberFormat = d3.format(' ^$,.r');
-    //            console.log(budgetData);
-    // normalize/parse data
-    //        budgetData.forEach(function(d) {
-    //            d.Spent = d.Spent.match(/\d+/);
-    //        });
 
     // SET CROSSFILTER
     var ndx = crossfilter(budgetData),
 
-    //          totalDim            = ndx.dimension(function(d) {return d.total;}),
         all                 = ndx.groupAll(),
         sumAll              = ndx.groupAll().reduceSum(function(d) {return +d.budget}),
 
-    //            FYDim             = ndx.dimension(function(d) {return +d.FY;}),
         budgetDim           = ndx.dimension(function(d) {return Math.floor(d.budget/1000000);}),
         deptDim             = ndx.dimension(function(d) {return d.Department_Title;}),
         charDim             = ndx.dimension(function(d) {return d.Character_Title;}),
         actDim              = ndx.dimension(function(d) {return d.SubObjTitle;}),
         fundDim             = ndx.dimension(function(d) {return d.Fund_Title;}),
 
-    //            spendPerFYDim     = FYDim.group().reduceSum(function(d) {return +d.budget;}),
         totalBudget         = budgetDim.group().reduceSum(function(d) {return +d.budget}),
         spendPerDeptDim     = deptDim.group().reduceSum(function(d) {return +d.budget;}),
         spendPerCharDim     = charDim.group().reduceSum(function(d) {return +d.budget;}),
@@ -53,11 +42,6 @@ var myData = d3.csv('data/14budget.csv', function(budgetData){
         .formatNumber(d3.format(" ^$,.r"))
         .valueAccessor(function(d) {return sumAll.value();})
         .group(spendPerDeptDim);
-//                .renderlet(function(chart){
-//                    if (deptRowChart.filters().length) {
-//                        $("#filters").text(deptRowChart.filters()[0]);
-//                    }
-//                });
 
     budgetRingChart
         .width(200).height(200)
@@ -71,13 +55,8 @@ var myData = d3.csv('data/14budget.csv', function(budgetData){
         .width(340).height(1100)
         .dimension(deptDim)
         .colors(['#008cba'])
-//                .x(d3.scale.log())
-//                    .base(2)
-//                    .domain([62000, 1600000000])
-//                    .range([0, 10000])
         .group(spendPerDeptDim)
         .margins({top: 0, right: 20, bottom: 20, left: 0})
-//                .renderVerticalGridLines([false]) <--broken method v1.7.3
         .ordering(function(d) { return -d.value; }) //this was buried in docs, not in API reference for v1.7.0, submitted PR
         .gap(3)
         .labelOffsetX([5])
@@ -89,9 +68,6 @@ var myData = d3.csv('data/14budget.csv', function(budgetData){
         .group(spendPerFundDim)
         .colors(['#008cba'])
         .ordering(function(d) { return -d.value; })
-//                .xUnits(dc.units.ordinal)
-//                .x(d3.scale.ordinal())
-////                .y(d3.scale.linear().domain([0, myData.length + 1]))
         .elasticX(true)
         .labelOffsetX([5])
         .labelOffsetY([20])
